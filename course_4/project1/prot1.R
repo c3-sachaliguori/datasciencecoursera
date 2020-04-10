baff7d9f8e1030ae28fe66f08b63ec82b2140336
Sacha Liguori <sacha.liguori@c3.ai>
2020-04-10 23:02:42 +0200

    Project course 4

--- course_4/project1/prot1.R
+++ course_4/project1/prot1.R
@@ -1,5 +1,23 @@
-# Title     : TODO
+# Title     : Plot 1
 # Objective : TODO
 # Created by: sachaliguori
 # Created on: 09/04/2020
 
+library("data.table")
+setwd('~/Coursera/Data_science/datasciencecoursera/course_4/project1')
+path <- file.path(getwd(), 'data')
+
+powerDT <- data.table::fread(input = "data/household_power_consumption.txt", na.strings="?")
+
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot1.png", width=480, height=480)
+
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
